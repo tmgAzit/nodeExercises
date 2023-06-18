c323cde4a49ec518ab872d772cb3f882d3feb3e4
tmgAzit <azit.ghising@gmail.com>
2023-06-18 22:57:43 +0930

    Create a fileStream server.

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-// http module
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  //   console.log(req);
-  if (req.url === '/') {
-    res.end('Welcome to the programming world!');
-    return;
-  }
-  if (req.url === '/about') {
-    res.end('This is about page.');
-    return;
-  }
-  res.end(`<h1> Oops!</h1> 
-  <p> The page you are looking for is unavailable.</p>
-  <a href="/"> back home</a>`);
-});
-
-server.listen(5000, () => console.log("I'am listening...."));
+const port = 5000;
+http
+  .createServer((req, res) => {
+    const fileStream = fs.createReadStream('./content/subfolder/big.txt');
+    fileStream.on('open', () => {
+      fileStream.pipe(res);
+    });
+    fileStream.on('error', (err) => {
+      res.end(err);
+    });
+  })
+  .listen(port);
--- fs-sync.js
+++ fs-sync.js
@@ -1,14 +1,21 @@
 // FS module - file systme
 
-const { log } = require('console');
-const fs = require('fs');
+// const { log } = require('console');
+// const fs = require('fs');
 
-const first = fs.readFileSync('./content/subfolder/text.txt', 'utf-8'); // fs.readFileSync('filepath', 'encoded');
-const second = fs.readFileSync('./content/subfolder/second.text', 'utf-8');
-console.log(first, second);
+// const first = fs.readFileSync('./content/subfolder/text.txt', 'utf-8'); // fs.readFileSync('filepath', 'encoded');
+// const second = fs.readFileSync('./content/subfolder/second.text', 'utf-8');
+// console.log(first, second);
 
-fs.writeFileSync(
-  './content/subfolder/third.txt',
-  `This is the text. ${first} ${second}`,
-  { flag: 'a' } // flag:'a' omit to override a new file, but add the line of text.
-);
+// fs.writeFileSync(
+//   './content/subfolder/third.txt',
+//   `This is the text. ${first} ${second}`,
+//   { flag: 'a' } // flag:'a' omit to override a new file, but add the line of text.
+// );
+
+const { writeFileSync } = require('fs');
+for (let i = 0; i <= 100000; i++) {
+  writeFileSync('./content/subfolder/big.txt', `hello world ${i}\n`, {
+    flag: 'a',
+  });
+}
